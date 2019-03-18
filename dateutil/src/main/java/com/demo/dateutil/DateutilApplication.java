6a083c2f9dc304953c2a2ba847a6e5106f732af9
caowen <1042879511@qq.com>
2019-03-18 09:52:43 +0800

    上传日期工具类

---
+++ dateutil/src/main/java/com/demo/dateutil/DateutilApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dateutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateutilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateutilApplication.class, args);
+	}
+
+}
